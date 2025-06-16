3e67fa277f79f8d95fac972de921b68c99ebaf81
niki <nikolett0203@gmail.com>
2025-06-15 22:58:43 -0400

    full responsive hero minus the moon section perhaps

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 import { spawnGalaxy } from './stars.js';
+import { zeroG } from './title.js';
 
 window.onload = function () {
     spawnGalaxy();
+    zeroG();
 }
---
+++ js/title.js
@@ -0,0 +1,32 @@
+export function zeroG () {
+
+    const title = document.getElementById('main-title');
+
+    if (title) {
+
+        // retrieve HTML text, then clear the whole tag / nested elements
+        const title_text = title.textContent;
+        title.innerHTML = '';
+
+        for (let i = 0; i < title_text.length; i++) {
+
+            // retrieve each letter and create a new span container to hold it
+            const letter = title_text[i];
+            const span = document.createElement('span');
+
+            // randomly generate attributes of the float animation
+            const duration = 4;
+            const delay = i * 0.3;
+            const index = (i % 6) + 1;
+
+            // add attributes to new span container
+            span.textContent = letter;
+            span.className = 'zeroG-letter';
+            span.style.animation = `float${index} ${duration}s ease-in-out infinite ${delay}s`;
+
+            // then add span to html
+            title.appendChild(span)
+        }
+    }
+}
+
