fc6b03c70d2b180e6e22ce9668175e18e7fb5082
niki <nikolett0203@gmail.com>
2025-06-17 21:07:01 -0400

    figured out the hover

--- js/nebula.js
+++ js/nebula.js
@@ -14,26 +14,101 @@ const centerY = () => canvas.height / 2;
 // define planet attributes
 // rx/y: horizontal/vertical radii of orbit, angle: starting angle of orbit in radians
 let planets = [
-    { rX: 160, rY: 90, speed: 0.006, angle: 1, size: 7, color: 'white', id: 1 },
-    { rX: 240, rY: 130, speed: 0.004, angle: 2, size: 10, color: 'white', id: 2 },
-    { rX: 320, rY: 180, speed: 0.0025, angle: 3.2, size: 12, color: 'white', id: 3 }
+    { rX: 160, rY: 90, speed: 0.006, angle: 1, size: 7, color: 'white', id: 1, hover: false, og_speed: 0.006 },
+    { rX: 240, rY: 130, speed: 0.004, angle: 2, size: 10, color: 'white', id: 2, hover: false, og_speed: 0.004 },
+    { rX: 320, rY: 180, speed: 0.0025, angle: 3.2, size: 12, color: 'white', id: 3, hover: false, og_speed: 0.0025 }
 ]
+
+// variables to store screen state
+let hovered_planet = null;
+let pause = false;
 
 // sets width and height of canvas equal to the size of the browser window
 function resize() {
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
 }
-window.addEventListener('resize', resize);
 
-export function spawnNebula() {
+function mousePos(e) {
 
-    // ensure canvas always covers screen by adding an event listener
-    resize();
+    // grabs position and size of canvas on screen
+    const rect = canvas.getBoundingClientRect();
 
-    draw();
+    // e.clientX is where mouse is horizontally on page
+    // rect.left is how far canvas is from left of the page
+    // so e.clientX - rect.left is how far the mouse is inside the canvas horizontally
+    return {
+        x: e.clientX - rect.left,
+        y: e.clientY - rect.top
+    };
+}
+
+// use sum of squares to calculate mouse distance from planets
+function distance (x1, y1, x2, y2) {
+    return Math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2);
+}
+
+// every time the mouse moves, check if it's close enough to a planet
+function mouseMove(e) {
+
+    const pos = mousePos(e);
+    let curr_hover = false;
+
+    planets.forEach(p => {
+
+        // cos(angle) gives us the length of the adjacent (cos = a / hyp)
+        // in radian unit circle, hypotenuse is 1 so when we multiply by p.rX we're scaling it to find horizontal x position
+        // vertical is same idea but with sin and finding the opposite (multiply by tilt to flatten, giving angled orbit effect)
+        const x = centerX() + p.rX * Math.cos(p.angle);
+        const y = centerY() + p.rY * Math.sin(p.angle) * tilt;
+
+        // calculate distance between mouse pos and planet coords
+        const dist = distance(pos.x, pos.y, x, y)
+
+        // was the planet previously hovered?
+        const prev_hover = p.hover;
+
+        // now store whether or not it is currently hovered
+        // if the distance from the cursor is smaller than the planet's size + a buffer, it is hovered
+        p.hover = dist < p.size + 10;
+
+        if (p.hover) {
+            curr_hover = true;
+            canvas.style.cursor = 'pointer';
+
+            if (!prev_hover) {
+                console.log('new hover')
+            }
+
+        } else if (prev_hover) {
+            console.log('done hover')
+        }
+
+    });
+
+    // don't need cursor to point if it's not pointed at anything
+    if (!curr_hover) {
+        canvas.style.cursor = 'default';
+    }
 
 }
+
+// reset everything after the mouse is no longer hovering
+function mouseLeave(e) {
+
+    planets.forEach(p => {
+
+        if (p.hover) {
+            p.hover = false;
+            // hide
+        }
+    });
+
+    hovered_planet = null;
+    canvas.style.cursor = 'default'
+
+}
+
 
 function draw() {
 
@@ -58,7 +133,9 @@ function draw() {
             false
         );
         // give a ghostly white hue to the orbital path
-        ctx.strokeStyle = 'rgba(255, 255, 255, 0.1)';
+        // if hovered, make it brighter and wider
+        ctx.strokeStyle = p.hover ? 'rgba(255, 255, 255, 0.3)' : 'rgba(255, 255, 255, 0.1)';
+        ctx.lineWidth = p.hover ? 2 : 1;
 
         // then render the ellipse onto the canvas
         ctx.stroke();
@@ -68,9 +145,6 @@ function draw() {
     // now we're drawing the planets themselves
     planets.forEach(p => {
 
-        // cos(angle) gives us the length of the adjacent (cos = a / hyp)
-        // in radian unit circle, hypotenuse is 1 so when we multiply by p.rX we're scaling it to find horizontal x position
-        // vertical is same idea but with sin and finding the opposite (multiply by tilt to flatten, giving angled orbit effect)
         const x = centerX() + p.rX * Math.cos(p.angle);
         const y = centerY() + p.rY * Math.sin(p.angle) * tilt;
 
@@ -82,8 +156,16 @@ function draw() {
         // style it a little bit
         ctx.fillStyle = p.color;
         ctx.shadowColor = p.color;
-        ctx.shadowBlur = 12;
+        ctx.shadowBlur = p.hover ? 25 : 12;
         ctx.fill();
+
+        // slow planet 10% when hovered
+        if (p.hover) {
+            p.speed = p.og_speed * 0.1; 
+        } else {
+            // restore original speed after hover is complete
+            p.speed = p.og_speed;
+        }
 
         // update position of planet for the next frame
         p.angle += p.speed;
@@ -93,3 +175,18 @@ function draw() {
     // as soon as frame is complete, draw the next one
     requestAnimationFrame(draw);
 }
+
+// event listeners
+window.addEventListener('resize', resize);
+canvas.addEventListener('mousemove', mouseMove);
+canvas.addEventListener('mouseleave', mouseLeave)
+console.log("Added")
+
+export function spawnNebula() {
+
+    // ensure canvas always covers screen by adding an event listener
+    resize();
+    draw();
+
+}
+
